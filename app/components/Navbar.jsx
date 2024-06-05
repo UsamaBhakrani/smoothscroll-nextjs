249493c2e88ea129304243965f43d1c3b68c9c43
Mohammed Usama <usamabhakrani@gmail.com>
2024-06-05 18:29:24 +0500

    Links Object

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,31 +1,26 @@
 import Link from "next/link";
 
 const Navbar = () => {
+  const links = [
+    { label: "Section 1", href: "section-1" },
+    { label: "Section 2", href: "section-2" },
+    { label: "Section 3", href: "section-3" },
+    { label: "Section 4", href: "section-4" },
+  ];
   return (
     <nav className="flex justify-between sticky top-0 h-10">
       <div>LOGO</div>
       <div>
         <ul className="flex flex-row">
-          <li>
-            <Link href="#section-1" data-to-scrollspy-id="section-1">
-              Section 1
-            </Link>
-          </li>
-          <li>
-            <Link href="#section-2" data-to-scrollspy-id="section-2">
-              Section 2
-            </Link>
-          </li>
-          <li>
-            <Link href="#section-3" data-to-scrollspy-id="section-3">
-              Section 3
-            </Link>
-          </li>
-          <li>
-            <Link href="#section-4" data-to-scrollspy-id="section-4">
-              Section 4
-            </Link>
-          </li>
+          {links.map(({ href, label }) => {
+            return (
+              <li key={href}>
+                <Link href={`#${href}`} data-to-scrollspy-id={href}>
+                  {label}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
--- app/components/Section.jsx
+++ app/components/Section.jsx
@@ -4,13 +4,13 @@ import ScrollSpy from "react-ui-scrollspy";
 const Section = () => {
   return (
     <ScrollSpy activeClass="active" useBoxMethod={false} scrollThrottle={50}>
-      <section className="bg-red-500 h-screen w-screen" id="section-1">
+      <section className="bg-red-500 h-96 w-screen" id="section-1">
         Section 01
       </section>
-      <section className="bg-purple-500 h-screen w-screen" id="section-2">
+      <section className="bg-purple-500 h-96 w-screen" id="section-2">
         Section 02
       </section>
-      <section className="bg-cyan-500 h-screen w-screen" id="section-3">
+      <section className="bg-cyan-500 h-96 w-screen" id="section-3">
         Section 03
       </section>
       <section className="bg-orange-500 h-screen w-screen" id="section-4">
